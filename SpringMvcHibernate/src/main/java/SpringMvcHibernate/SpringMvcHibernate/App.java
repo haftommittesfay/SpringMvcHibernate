4a46aab4d80a993a7e02a122f5b7ecba2712f65a
halemayehu <halemayehu@bres.shl.uiowa.edu>
2018-07-11 16:34:04 -0500

    SpringMvc, Hibernate, JSP, MySQL

---
+++ SpringMvcHibernate/src/main/java/SpringMvcHibernate/SpringMvcHibernate/App.java
@@ -0,0 +1,13 @@
+package SpringMvcHibernate.SpringMvcHibernate;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
